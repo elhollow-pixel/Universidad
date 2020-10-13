a0b2262a0a5685b26317bef278208b1a24d89c30
elhollow-pixel <56979043+elhollow-pixel@users.noreply.github.com>
2020-10-13 11:38:46 -0500

    boton para regresar a pantalla principal desde Listado

--- app/src/main/java/co/edu/unimagdalena/apmoviles/universidad/ListadoActivity.java
+++ app/src/main/java/co/edu/unimagdalena/apmoviles/universidad/ListadoActivity.java
@@ -7,18 +7,21 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ListadoActivity extends AppCompatActivity {
+public class ListadoActivity extends AppCompatActivity implements View.OnClickListener {
 
     ListView listado;
     EstudianteController estudianteController;
+    Button agregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado);
+        agregar = findViewById(R.id.btnAgregar);
         listado = findViewById(R.id.lstlistado);
         estudianteController = new EstudianteController(this);
         Cursor c = estudianteController.allEstudiantes2();
@@ -30,12 +33,23 @@ public class ListadoActivity extends AppCompatActivity {
                 TextView cod = view.findViewById(R.id.txtcodigo);
                 TextView nom = view.findViewById(R.id.txtnombre);
                 TextView prom = view.findViewById(R.id.txtprograma);
-                Intent i = new Intent(getApplicationContext(),ModficarActivity.class);
-                i.putExtra("cod",cod.getText().toString());
-                i.putExtra("nom",nom.getText().toString());
-                i.putExtra("prog",prom.getText().toString());
-                startActivity(i);
+                Intent l = new Intent(getApplicationContext(),ModficarActivity.class);
+                l.putExtra("cod",cod.getText().toString());
+                l.putExtra("nom",nom.getText().toString());
+                l.putExtra("prog",prom.getText().toString());
+                startActivity(l);
             }
         });
+        agregar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnAgregar:
+                Intent lanzar  = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(lanzar);
+                break;
+        }
     }
 }
